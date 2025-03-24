f72e47584465428f7fef0326a26262341e348ab1
yachimiya <github.grandkid494@passmail.net>
2025-03-24 17:03:21 +0900

    first commit

---
+++ src/main/java/dev/poporo/labgithubactionstrivy/LabGithubActionsTrivyApplication.java
@@ -0,0 +1,13 @@
+package dev.poporo.labgithubactionstrivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGithubActionsTrivyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGithubActionsTrivyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/poporo/labgithubactionstrivy/LabGithubActionsTrivyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.poporo.labgithubactionstrivy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGithubActionsTrivyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
