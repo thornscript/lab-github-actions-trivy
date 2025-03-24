4a0b33174391b24f2553e345e8b99806825ff737
thornscript <8318212+thornscript@users.noreply.github.com>
2025-03-25 03:58:29 +0900

    PR 데코 테스트 (#1)
    * 주석을 수정함
    
    * PR 정보를 같이 넘김
    
    * PR 정보를 같이 넘김
    
    * Jacoco 설정 추가함 - SonarQube에 적용되는지 확인
    
    * Jacoco 설정 추가함 - SonarQube에 적용되는지 확인
    
    * Jacoco 설정 추가함 - SonarQube에 적용되는지 확인
    
    * PR 정보 다시 전달함 - SonarQube에 적용되는지 확인

--- src/main/java/dev/poporo/labgithubactionstrivy/service/WeakEncryptionService.java
+++ src/main/java/dev/poporo/labgithubactionstrivy/service/WeakEncryptionService.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class WeakEncryptionService {
 
+    // 약한 해시 알고리즘
     public String encrypt(String data) throws Exception {
-        MessageDigest md = MessageDigest.getInstance("MD5");  // 약한 해시 알고리즘
+        MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] hash = md.digest(data.getBytes());
         return Base64.getEncoder().encodeToString(hash);
     }
